e6c4779326a0b68af81f46275929c58a6b985e78
wassim <wassim.sboui@gmail.com>
2020-03-28 12:10:32 +0100

    CI

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -8,10 +8,7 @@ describe('workspace-project App', () => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
-    page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to coronavirus!');
-  });
+  
 
   afterEach(async () => {
     // Assert that there are no errors emitted from the browser
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,9 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'coronavirus'`, () => {
+  it(`should have as title 'Wassim'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('coronavirus');
-  });
-
-  it('should render title in a h1 tag', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to coronavirus!');
+    expect(app.title).toEqual('Wassim');
   });
 });
